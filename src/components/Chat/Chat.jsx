a56e2f6214eeb605dd8a081b99f5bba0af7895b9
AnuragOP01 <ranurag404@gmail.com>
2024-09-02 21:12:03 +0530

    second commit

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -21,7 +21,7 @@ const Chat = () => {
   const [users, setUsers] = useState("");
   const [message, setMessage] = useState("");
   const [messages, setMessages] = useState([]);
-  const ENDPOINT = "http://localhost:5000/";
+  const ENDPOINT = "https://chatbackend-84xx.onrender.com/";
   const [light, setLight] = useState(true);
   const change = () => {
     setLight(!light);
